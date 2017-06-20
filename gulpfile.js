76d5a16ff760ac12e7f0d5a08bd8337e8e35bf70
akkey247 <akkey52417@gmail.com>
2017-06-20 23:51:13 +0900

    electron-packagerを追加、gulpでパッケージ化タスクを登録

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp');
+var packager = require('electron-packager');
+
+// パッケージ作成(Windows)
+gulp.task('package:win32', function (done) {
+  packager({
+    dir: './',             // アプリケーションのパッケージとなるディレクトリ
+    out: './dist',            // .app や .exeの出力先ディレクトリ
+    name: 'ElectronApp',      // アプリケーション名
+    arch: 'x64',              // CPU種別. x64 or ia32
+    platform: 'win32',        // OS種別. darwin or win32 or linux
+    electron-version: '1.4.13',        // Electronのversion
+    overwrite: true           // 上書き
+  }, function (err, path) {
+    // 追加でパッケージに手を加えたければ, path配下を適宜いじる
+    done();
+  });
+});
