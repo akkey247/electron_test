93cfa6b418253672910aadb1dbbba41c2d0cf247
akkey247 <akkey52417@gmail.com>
2017-07-01 19:44:26 +0900

    パッケージ化処理を追加(一時コミット)

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var gulp = require('gulp');
+var packager = require('electron-packager');
+
+// パッケージ作成(Windows)
+gulp.task('package:win32', function (done) {
+  packager({
+    dir: './',                 // アプリケーションのパッケージとなるディレクトリ
+    out: './dist',             // .app や .exeの出力先ディレクトリ
+    name: 'IPC-Connect',       // アプリケーション名
+    arch: 'x64',               // CPU種別. x64 or ia32
+    platform: 'win32',         // OS種別. darwin or win32 or linux
+    electronVersion: '1.6.11', // Electronのversion
+    overwrite: true,           // 上書き
+    // 無視ファイル
+    ignore: "/src",
+  }, function (err, path) {
+    // 追加でパッケージに手を加えたければ, path配下を適宜いじる
+    done();
+  });
+});
--- src/renderer/components/Root.jsx
+++ src/renderer/components/Root.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 
 export default class Root extends React.Component {
